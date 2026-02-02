0b10ade9a161c3513b4144922db7bac69414a935
ZHEKA <Chernik.dota@mail.ru>
2026-02-02 03:53:32 +0300

    зарегестрирована модель постов в админке

--- First_project/blog/admin.py
+++ First_project/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+# Регистрируем модель в админке Django
+@admin.register(models.PostFilesModel)
+class PostFileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'code', 'download_count',)  # Какие поля показывать в списке объектов
+    search_fields = ('title',)  # По каким полям можно искать
+    readonly_fields = ('download_count',)  # Поле, которое видно, но нельзя редактировать
+
+    # exclude = ('download_count',)  # Какие поля скрыть при создании/редактировании
