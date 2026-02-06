60507abb54768d8758ecaca9cd5b11d889828064
ZHEKA <Chernik.dota@mail.ru>
2026-02-07 02:13:36 +0300

    22) добавлены представления для страницы поста, подредактирована категория, сделаны шаблон(ы)

--- First_project/blog/views.py
+++ First_project/blog/views.py
@@ -13,9 +13,24 @@ def index(request):
 
 def category_page(request, pk, slug):
     category = models.CategoryModel.objects.get(pk=pk)
+    descendant_categories = category.get_descendants(include_self=True)
+    posts = models.PostModel.objects.filter(category__in=descendant_categories)
+
     context = {
         'category': category,
+        'posts': posts,
     }
 
     return render(request, 'blog/category_page.html', context)
 
+
+def post_page(request, pk, slug):
+    post = models.PostModel.objects.get(pk=pk)
+    post.views += 1
+    post.save()
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'blog/post_page.html', context)
