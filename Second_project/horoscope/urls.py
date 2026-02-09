0054a6578e7c610379a2c3ac0f84bc4c1c89f57e
ZHEKA <Chernik.dota@mail.ru>
2026-02-10 02:20:21 +0300

    Заменены на динамические urлы знаки-зодиаки)

--- Second_project/horoscope/urls.py
+++ Second_project/horoscope/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'horoscope'
 
 urlpatterns = [
-    path('leo/', views.leo),
-    path('scorpion/', views.scorpion),
+    path('<sign_zodiac>/', views.get_info_about_zodiac_sign),
+    path('sign/<int:pk>/', views.sign),
 
+    # path('leo/', views.leo),
+    # path('scorpion/', views.scorpion),
 ]
--- Second_project/horoscope/views.py
+++ Second_project/horoscope/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
@@ -9,3 +9,24 @@ def leo(request):
 
 def scorpion(request):
     return HttpResponse('Знак зодиака scorpion')
+
+def sign(request, pk):
+    context = {
+        'pk':pk
+    }
+    return HttpResponse(f'{context} fsfa')
+
+def get_info_about_zodiac_sign(request, sign_zodiac):
+    zodiac_descriptions = {
+        'leo': 'Знак зодиака лев',
+        'scorpion': 'Знак зодиака скорпион',
+        'taurus': 'Знак зодиака телец',
+        # ... добавитьь остальные 9 знаков
+    }
+
+    description = zodiac_descriptions.get(sign_zodiac.lower())
+
+    if description:
+        return HttpResponse(description)
+    else:
+        return HttpResponseNotFound(f'Неизвестный знак зодиака {sign_zodiac}')
