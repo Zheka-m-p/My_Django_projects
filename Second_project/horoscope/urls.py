aea8a5f97125a32496bbb22f5f3e93c591ae158b
ZHEKA <Chernik.dota@mail.ru>
2026-02-10 03:24:45 +0300

    fix некоторые ненужные представления, перепесины другие, динамеческие маршруты по int, str. функции можно объединить две в одну, но пока пусть так

--- Second_project/horoscope/urls.py
+++ Second_project/horoscope/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'horoscope'
 
 urlpatterns = [
-    path('<sign_zodiac>/', views.get_info_about_zodiac_sign),
-    path('sign/<int:pk>/', views.sign),
+    path('<int:number_zodiac>/', views.get_info_about_zodiac_sign_by_number),
+    path('<str:sign_zodiac>/', views.get_info_about_zodiac_sign),
 
-    # path('leo/', views.leo),
-    # path('scorpion/', views.scorpion),
 ]
--- Second_project/horoscope/views.py
+++ Second_project/horoscope/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+zodiac_descriptions = {
+    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
+    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
+    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
+    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
+    'leo': ' Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
+    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
+    'libra': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
+    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
+    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
+    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
+    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
+    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+}
+
+zodiac_numbers = dict(zip(range(1, 13), list(zodiac_descriptions.keys())))
+
 # Create your views here.
+def get_info_about_zodiac_sign_by_number(request, number_zodiac):
+    zodiac = zodiac_numbers.get(number_zodiac) # имя зодиак-строку получаем
+    if zodiac:
+        description = zodiac_descriptions.get(zodiac.lower())
+        return HttpResponse(description)
+    else:
+        return HttpResponseNotFound(f'Неизвестный номер зодиака - {number_zodiac}')
 
-def leo(request):
-    return HttpResponse('Знак зодиака лев')
 
-
-def scorpion(request):
-    return HttpResponse('Знак зодиака scorpion')
-
-def sign(request, pk):
-    context = {
-        'pk':pk
-    }
-    return HttpResponse(f'{context} fsfa')
-
-def get_info_about_zodiac_sign(request, sign_zodiac):
-    zodiac_descriptions = {
-        'leo': 'Знак зодиака лев',
-        'scorpion': 'Знак зодиака скорпион',
-        'taurus': 'Знак зодиака телец',
-        # ... добавитьь остальные 9 знаков
-    }
-
+def get_info_about_zodiac_sign(request, sign_zodiac): # можно совместить с вьюхой выше, но пока пусть так
     description = zodiac_descriptions.get(sign_zodiac.lower())
-
     if description:
         return HttpResponse(description)
     else:
