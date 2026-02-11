cbfad8e0168cc2e1727c2906f4d73bd942d5cfe4
ZHEKA <Chernik.dota@mail.ru>
2026-02-11 05:14:37 +0300

    добавлено описание какой контекст мы ищем и потом передаем

--- Second_project/horoscope/views.py
+++ Second_project/horoscope/views.py
@@ -6,7 +6,7 @@ from .data_zodiac import data_zodiac, types_zodiac_dict
 
 # Create your views here.
 def index_horoscope(request):
-    data = data_zodiac
+    data = data_zodiac # передали весь список словарей в дату
     context = {
         'data': data
     }
@@ -14,7 +14,7 @@ def index_horoscope(request):
 
 
 def get_info_about_sign_zodiac(request, sign_zodiac):
-    data = [elem for elem in data_zodiac if elem['name'] == sign_zodiac]
+    data = [elem for elem in data_zodiac if elem['name'] == sign_zodiac] # получаем конкретный словарь знака зодиака
     if not data:
         return HttpResponseNotFound(f'<h2>Неизвестный знак зодиака: {sign_zodiac}</h2>')
     context = {
@@ -24,14 +24,14 @@ def get_info_about_sign_zodiac(request, sign_zodiac):
 
 
 def get_info_about_sign_zodiac_by_number(request, number_zodiac):
-    data = [elem for elem in data_zodiac if elem['number'] == number_zodiac]
+    data = [elem for elem in data_zodiac if elem['number'] == number_zodiac] # так же знак зодиака, но уже по номеру
     if not data:
         return HttpResponseNotFound(f'<h2>Неизвестный номер зодиака: {number_zodiac}</h2>')
     return redirect('horoscope:sign_zodiac', sign_zodiac=data[0]['name'])
 
 
 def type_zodiac(request):
-    data = list(types_zodiac_dict)
+    data = list(types_zodiac_dict) # 4 типа стихий получили (из ключей словаря)
     context = {
         'data': data
     }
@@ -39,16 +39,10 @@ def type_zodiac(request):
 
 
 def type_element(request, type_element):
-    data = [elem for elem in data_zodiac if elem['element'] == type_element]
+    data = [elem for elem in data_zodiac if elem['element'] == type_element] # словари, с переданным парамером стихии
     if not data:
         return HttpResponseNotFound(f'Неизвестный тип элемента: {type_element}')
     context = {
         'data': data
     }
     return render(request, 'horoscope/type_element.html', context)
-
-
-
-
-
-
