75a3696f1156f2790391563f4ddf3edd86e8b869
ZHEKA <Chernik.dota@mail.ru>
2026-02-14 09:52:27 +0300

    добавлен альтернативный вариант ссыл на соло_муви, get_absolute_url - метод в модели Movie

--- Second_project/movie_app/models.py
+++ Second_project/movie_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse # полезная штука, но вечно забываю...
 
 # Create your models here.
 class Movie(models.Model):
@@ -8,6 +9,9 @@ class Movie(models.Model):
     year = models.IntegerField(verbose_name='год выпуска', null=True)
     budget = models.IntegerField(verbose_name='бюджет фильма', default=1000000)
 
+    def get_absolute_url(self):
+        '''Для получения ссылки на наш одиночный фильм'''
+        return reverse('movie:solo_movie', args=[self.id])
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
