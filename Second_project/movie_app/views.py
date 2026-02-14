481ea647e7adc3c8e2513ade9c36913da6ae53dd
ZHEKA <Chernik.dota@mail.ru>
2026-02-15 02:30:15 +0300

    Немного подкоректированы шаблоны, для красоты(но еще не до конца), добавлены функции для разного возможного вывода данных из таблицы, добавлена общая статистика по всем фильмам

--- Second_project/movie_app/views.py
+++ Second_project/movie_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+# функции для выборки, по сути всё как в SQL, + для сотировки (+asc, desc)
+from django.db.models import F, Min, Max, Count, Avg
 
 # Create your views here.
 def show_all_movie(request):
     movies = Movie.objects.all()
+    stats = movies.aggregate(avg_budget=Avg('budget'), max_rating=Max('rating'),
+                             min_year=Min('year'), max_year=Max('year'), count_film=Count('id'))
+    # movies = Movie.objects.order_by('-rating', 'budget')[:2] # способ сортировки по полям
     context = {
-        'movies': movies
+        'movies': movies,
+        'stats' : stats
     }
     return render(request, 'movie_app/all_movies.html', context=context)
 
