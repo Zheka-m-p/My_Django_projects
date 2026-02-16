cf99d705b755f46c82702dc3a7204b7663e4dee0
ZHEKA <Chernik.dota@mail.ru>
2026-02-16 05:32:42 +0300

    добавлен ручной фильтр в админке, аналог(точнее еще один) из поля list_filter= , работает, но вряд-ли я это запомню)...

--- Second_project/movie_app/admin.py
+++ Second_project/movie_app/admin.py
@@ -3,11 +3,35 @@ from . models import Movie
 from django.db.models import QuerySet
 
 # Register your models here.
+class RatingMovieFilter(admin.SimpleListFilter):
+    title = 'Рейтинг:'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('меньше 40-ка', 'Низкий'), # (значение, название_значения)
+            ('от 40 до 59', 'Средний'), # (значение, название_значения)
+            ('от 60 до 80',  'Высокий'), # (значение, название_значения)
+            ('Больше 80-ти',  'Топчик'), # (значение, название_значения)
+        ]
+
+    def queryset(self, request, queryset: QuerySet): # QuerySet - коллекция всех записей муви
+        if self.value() == 'меньше 40-ка': # проверям на равенство значению, которые мы сами задани
+            return queryset.filter(rating__lt=40)
+        elif self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40, rating__lte=59)
+        elif self.value() == 'от 60 до 80':
+            return queryset.filter(rating__gte=60, rating__lte=80)
+        elif self.value() == 'Больше 80-ти':
+            return queryset.filter(rating__gt=80)
+        return queryset# ← если не выбран фильтр
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('name', 'rating', 'year', 'budget', 'currency','rating_status')  # добавляем нужные нам колонки для отображения на сайте
     search_fields = ('name', 'year',)  # добавляем поле поиска по данным колонкам
-    list_filter = ('budget', 'year',)  # добавляем фильтр по данной колонке, можем добавлять несколько фильтров
+    list_filter = ('budget', 'currency', RatingMovieFilter)  # добавляем фильтр по данной колонке, можем добавлять несколько фильтров
 
     list_editable = ( 'rating', 'year', 'budget', 'currency',) # чтобы изменять на месте, не переходя на конкретный фильм
     # ordering = ('name', ) # сортировать по имени фильма
