59b8a5b1088b368649c437250c584db39235fc46
ZHEKA <Chernik.dota@mail.ru>
2026-02-17 20:47:30 +0300

    форма с POST-запросом/методом, небольшая минимальная проверка, но это формы вручную, чисто на html

--- Second_project/feedback/urls.py
+++ Second_project/feedback/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'feedback' # чисто название, может быть любым, для удобства навигации по url-ам
 
 urlpatterns = [
+    path('done/', views.done, name='done'),
     path('', views.index, name='home'),
     # path('hello/', views.hello, name='hello'),
 
--- Second_project/feedback/views.py
+++ Second_project/feedback/views.py
@@ -4,11 +4,21 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request, 'feedback/home_feedback.html')
+    if request.method == 'POST':
+        name = request.POST.get('name', 'unknown_name')
+        print(f'имя: {name}')
+        if len(name) == 0: # минимальная обработка формы
+            return render(request, 'feedback/home_feedback.html', context={'got_error': True})
+        return HttpResponseRedirect(reverse('feedback:done')) # при редиректе теряются данные
+
+    return render(request, 'feedback/home_feedback.html', context={'got_error': False})
+
+def done(request):
+    return render(request, 'feedback/done.html')
 
 
 def hello(request):
     res = request.GET # словарь
     print(res.get('name', 'кто ты?'))
-    return HttpResponseRedirect(reverse('feedback:home'))
+    return HttpResponseRedirect(reverse('feedback:home')) # при редиректе теряются данные
     # return render(request, 'feedback/home_feedback.html')
