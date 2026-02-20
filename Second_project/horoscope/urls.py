03a959d9dde4e5dce583c62b8cf41427bb0c32dd
ZHEKA <Chernik.dota@mail.ru>
2026-02-20 16:42:19 +0300

    Так...гороскоп настроен, кое-как. вроде бы все +- красиво работает, насколько возможно) Осталось добавить логику на лайки, комменты под постами, возможностть автору поста удалять комментарии, хе хе

--- Second_project/horoscope/urls.py
+++ Second_project/horoscope/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # переходим на шаблоны
     path('type/', views.type_zodiac, name='types_element_zodiac'),
     path('type/<str:type_element>/', views.type_element, name='type_element'),
-
+    # соло зодиаки по названию или по номеру
     path('<int:number_zodiac>/', views.get_info_about_sign_zodiac_by_number, name='number_zodiac'),#
     path('<str:sign_zodiac>/', views.get_info_about_sign_zodiac, name='sign_zodiac'),#
 
