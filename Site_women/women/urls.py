2163d1a640263249bd5f96d5879c5b83420e525a
Evgeniy <Chernik.dota@mail.ru>
2026-05-27 22:23:21 +0300

    Добавлены динамические маршруты в urls, int, slug, re_path, собственный конвертор

--- Site_women/women/urls.py
+++ Site_women/women/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
 
-
+# noinspection PyTypeChecker
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('cats/', views.categories, name='cats'),
+    path('', views.index, name='index'), # http//127.0.0.1:800/
+    path('cats/<int:cat_id>/', views.categories, name='cats'), # http//127.0.0.1:800/cats/2
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cat_detail'), # http//127.0.0.1:800/cats/dd22
+
+    path('archive/<year4:year>/', views.archive, name='archive'),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive, name='archive'),
 
 ]
