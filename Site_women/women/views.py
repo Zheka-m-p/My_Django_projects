afb0d5ff8c3126c38fccf6c1dcdf78f153b97550
Evgeniy <Chernik.dota@mail.ru>
2026-05-27 03:15:20 +0300

    add two views, create urls.py

--- Site_women/women/views.py
+++ Site_women/women/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request): # request - это ссылка на класс НttpRequest, содержит инфу о запросе
+    return HttpResponse("Страница приложения women")
+
+def categories(request):
+    return HttpResponse('<h1>Статьи по категории</h1>')
